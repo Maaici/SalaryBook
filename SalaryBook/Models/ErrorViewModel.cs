6e6aefcfc9b018537569a6ff07ed4f27fe602612
Maaici <724556872@qq.com>
2020-01-08 08:59:32 +0800

    添加项目

---
+++ SalaryBook/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalaryBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
